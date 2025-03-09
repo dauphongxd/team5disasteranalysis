d435525933a3e3b127f2b8ed54fccfea962f646b
Phong Dau <dauphongxd@gmail.com>
2025-03-09 18:41:55 -0500

    Add Three.js background scene

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,29 @@ import Filters from "./components/Filters";
 import MapSection from "./components/MapSection";
 import TweetFeed from "./components/TweetFeed";
 import HelpSection from "./components/HelpSection";
+import Scene from "./components/Scene"; // Import the Three.js background
 import "./styles.css";
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Filters />
-      <div className="main-content">
-        <MapSection />
-        <TweetFeed />
+    <>
+      {/* Render the Three.js background */}
+      <Scene />
+
+      {/* Main application content */}
+      <div className="container">
+        <div class="earth-container">
+            <div class="earth"></div>
+        </div>
+        <Header />
+        <Filters />
+        <div className="main-content">
+          <MapSection />
+          <TweetFeed />
+        </div>
+        <HelpSection />
       </div>
-      <HelpSection />
-    </div>
+    </>
   );
 }
 
