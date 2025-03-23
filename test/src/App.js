9fd47a0d21179b978c8461568e6ebd059c67562d
Jiwooow <jiwooerase@gmail.com>
2025-03-22 23:06:57 -0500

    Add files via upload

--- test/src/App.js
+++ test/src/App.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Filters from "./components/Filters";
 import MapSection from "./components/MapSection";
 import TweetFeed from "./components/TweetFeed";
 import HelpSection from "./components/HelpSection";
 import Scene from "./components/Scene"; // Import the Three.js background
+import Timechart from "./components/Timechart";
 import "./styles.css";
 
 function App() {
+  const [disasterData, setDisasterData] = useState([]); // State to store JSON data
+  const [selectedDisaster, setSelectedDisaster] = useState("all");
+
+  useEffect(() => {
+    // Fetch the JSON file from the public folder
+    fetch("/posts.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch disaster data");
+        }
+        return response.json();
+      })
+      
+      .then((data) => {
+        const filteredData = data.filter((post) => post.is_disaster === true);
+        setDisasterData(filteredData);
+      })
+      .catch((error) => {
+        console.error("Error fetching disaster data:", error);
+      });
+  }, []); // Runs once when the component mounts
+
+  console.log("Currently Selected Disaster:", selectedDisaster);
+
   return (
     <>
       {/* Render the Three.js background */}
@@ -19,12 +44,15 @@ function App() {
             <div class="earth"></div>
         </div>
         <Header />
-        <Filters />
+        <Filters setSelectedDisaster={setSelectedDisaster} />
         <div className="main-content">
           <MapSection />
           <TweetFeed />
         </div>
-        <HelpSection />
+        <div>
+          <Timechart disasterData={disasterData} selectedDisaster={selectedDisaster} />
+        </div>
+        <HelpSection disasterType={selectedDisaster} />
       </div>
     </>
   );
