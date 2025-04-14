165b451deb8c8b335a7161c1febaa837b17219de
Phong Dau <dauphongxd@gmail.com>
2025-04-14 16:24:16 -0500

    Switch from feed search to keywords search

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("disaster_feed.log", encoding='utf-8'),
+        # logging.FileHandler("disaster_feed.log", encoding='utf-8'),
         logging.StreamHandler()
     ]
 )
